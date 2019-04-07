821062f6f02637bf7d1af6ce71a018bccd8f9334
Bernd Goetz <bernd_goetz@swissre.com>
2019-04-07 23:47:49 +0200

    actuator security fix

--- src/main/java/com/example/demo/ActuatorSecurity.java
+++ src/main/java/com/example/demo/ActuatorSecurity.java
@@ -1,21 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 @Configuration
 @Order(10)
 public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
                 .anyRequest().hasRole("admin")
                 .and()
-                .httpBasic();
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
     }
 
 }
